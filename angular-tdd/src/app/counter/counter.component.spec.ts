ef482648a51e7a89172582c7daf3e2e6705a732a
W. Guilherme <wguilhermecriativo@gmail.com>
2020-11-29 23:22:14 -0300

    add: counter component + functions

---
+++ angular-tdd/src/app/counter/counter.component.spec.ts
@@ -0,0 +1,7 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CounterComponent } from './counter.component';
+
+describe('CounterComponent', () => {
+
+});
---
+++ angular-tdd/src/app/counter/counter.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss']
+})
+export class CounterComponent implements OnInit {
+  counter: number;
+
+  constructor() {
+    this.counter = 1;
+  }
+
+  increment() {
+    this.counter++
+  }
+  decrement() {
+    this.counter--
+  }
+
+  ngOnInit(): void {
+  }
+
+}
